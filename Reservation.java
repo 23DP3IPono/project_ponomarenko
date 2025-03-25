5087860bc1dac8a4a6f192b039d32017fb596567
Ivans Ponomarenko <166009312+23DP3IPono@users.noreply.github.com>
2025-03-25 09:32:35 +0000

    Update

--- Reservation.java
+++ Reservation.java
@@ -21,5 +21,39 @@ public class Reservation {
         return roomNumber;
     }
 
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public LocalDate getCheckOuDate() {
+        return checkOutDate;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "guestName='" + guestName + '\'' +
+                ", roomNumber=" + roomNumber +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                '}';
+    }
+
     
 }
--- ReservationManager.java
+++ ReservationManager.java
@@ -1,3 +1,23 @@
-public class ReservationManager {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ReservationManager {
+    private List<Reservation> reservations;
+
+    public ReservationManager() {
+        this.reservations = new ArrayList<>();
+    }
+
+    public void addReservation(String guestName, int roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        reservations.add(new Reservation(guestName, roomNumber, checkIn, checkOut));
+    }
+
+    public boolean removeReservation(int roomNumber) {
+        return reservations.removeIf(reservation -> reservation.getRoomNumber() == roomNumber);
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
 }
